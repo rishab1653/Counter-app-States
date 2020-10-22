3e4e43b1e081bd46b2dcaf77493bf476437668d1
rishab1653 <rishab959290@gmail.com>
2020-10-22 11:30:52 +0530

    SEt the max snd min limit of counter

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = ()=>{
        </header>
        <h2>Currrent value of count is {count}</h2>
        <button onClick={()=> setCount(0)}>Reset Counter</button>
-       <button onClick={()=> setCount(count + 1)}>Increment Counter</button>
-       <button onClick={()=> setCount(count - 1)}>Decrement Counter</button>
+       <button onClick={()=> (count>=10?console.log("Sorry Reached your maximum limit"):setCount(count + 1))}>Increment Counter</button>
+       <button onClick={()=> (count==0 ? console.log("Sorry Value cant be less than zero"):setCount(count - 1))}>Decrement Counter</button>
     </div>
   )
 }
