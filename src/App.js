6a8340aa9de0e09919378f2f236a1fe1004be006
rishab1653 <rishab959290@gmail.com>
2020-10-22 10:59:55 +0530

    added es7 hooks method to counter application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React , {useState} from 'react'
+import "./App.css"
+const App = ()=>{
+  const [count , setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <header>
+       <h1>Counter React Application using functional state </h1>
+       </header>
+       <h2>Currrent value of count is {count}</h2>
+       <button onClick={()=> setCount(0)}>Reset Counter</button>
+       <button onClick={()=> setCount(count + 1)}>Increment Counter</button>
+       <button onClick={()=> setCount(count - 1)}>Decrement Counter</button>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
